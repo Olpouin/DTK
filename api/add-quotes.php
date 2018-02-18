6c737c7228118b72a5a3a2516e31ab3af59f4a10
Olpouin <olpouin@live.fr>
2018-02-18 01:52:50 +0100

    Add quotes (Old one, need updates)

---
+++ api/add-quotes.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+$user = "olpouin";
+$pass = "password here";
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=olpouin;charset=utf8',$user,$pass);
+} catch (Exception $e) {
+	die('Error : '.$e->getMessage());
+}
+
+$captcha = $_GET['captcha'];
+$text = $_GET['text'];
+if(!isset($text)) {
+	$answer = 1;
+} elseif(!isset($captcha)) {
+	$answer = 2;
+} elseif(strlen($text) > 2000) {
+	$answer = 3;
+} elseif(strlen($text) < 10) {
+	$answer = 4;
+} elseif(strtolower($captcha) != "soké") {
+	$answer = 5;
+} else{
+	$answer = 0;
+	$req = $bdd->prepare('INSERT INTO DTK(text) VALUES(:text)');
+	$req->execute(array(
+		'text' => $text
+		)
+	);
+}
+
+$result = array("answer"=>$answer, "text"=>$text, "text_chars"=>strlen($text), "captcha"=>$captcha);
+echo json_encode($result);
+?>
